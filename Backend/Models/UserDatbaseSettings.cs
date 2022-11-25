e8feb05167f55dce5789a415def8a1e6e40d6156
Kolacs <71070272+Kolacs@users.noreply.github.com>
2022-11-25 19:23:34 +0100

    Folder structure

---
+++ Backend/Models/UserDatbaseSettings.cs
@@ -0,0 +1,8 @@
+namespace ApiToDatabase.Models;
+
+public class UserDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+    public string UsersCollectionName { get; set; } = null!;
+}
---
+++ Backend/Services/UserServicePostgres.cs
@@ -0,0 +1,40 @@
+﻿using ApiToDatabase.Data;
+using ApiToDatabase.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+
+namespace ApiToDatabase.Services
+{
+    public class UserServicePostgres : IUserService
+    {
+        UserDbContext _context;
+        public UserServicePostgres(UserDbContext context)
+        {
+            _context= context;
+        }
+        public async Task CreateUserAsync(User newUser)
+        { 
+            await _context.Users.AddAsync(newUser);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<User?> GetUserAsync(string id)
+            => await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+        public async Task<List<User>> GetUsersAsync()
+            => await _context.Users.ToListAsync();
+
+        public async Task RemoveUserAsync(string id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+        }
+
+        public async Task UpdateUserAsync(string id, User updatedUser)
+        {
+            var user =  await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+            _context.Users.Update(updatedUser);
+           await _context.SaveChangesAsync();
+        }
+    }
+}
