a18c54495e806f1ec6f8d8370c149b4360d26d01
Kolacs <71070272+Kolacs@users.noreply.github.com>
2022-11-25 19:24:08 +0100

    Folder structure

--- Models/UserDatbaseSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace ApiToDatabase.Models;
-
-public class UserDatabaseSettings
-{
-    public string ConnectionString { get; set; } = null!;
-    public string DatabaseName { get; set; } = null!;
-    public string UsersCollectionName { get; set; } = null!;
-}
