91abab2082031d4d99e6815bf4956d02cf179ed5
Kolacs <71070272+Kolacs@users.noreply.github.com>
2022-11-27 13:40:32 +0100

    Fixed styling

--- Client/app.js
+++ Client/app.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('myForm');
+const form = document.getElementById("myForm");
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
@@ -6,29 +6,17 @@ form.addEventListener('submit', function (e) {
     const payload = new FormData(form);
 
     console.log([...payload]);
-
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    headers.append('Accept', 'application/json');
   
-    //headers.append('Access-Control-Allow-Origin', "https://localhost:7019/api");
-    headers.append('Access-Control-Allow-Credentials', '*');
-  
-    headers.append('GET', 'POST', 'OPTIONS');
     fetch("https://localhost:7019/api",{
-        //credentials: 'include',
-        //mode : "no-cors",
         method : "POST",
-        body: JSON.stringify({
+        body : JSON.stringify({
             "username" : payload.get("username"),
             "password" : payload.get("password")
         }),
         headers : {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            //'Access-Control-Allow-Credentials': 'true'
-          }
-    })
+            "Accept" : "application/json",
+            "Content-Type" : "application/json"
+        }})
         .then(res => res.json())
         .then(data => console.log(data))
         .catch(err => console.log(err));
