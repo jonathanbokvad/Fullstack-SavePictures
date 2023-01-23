85f7aecad24fb4eb5f1cd31ee387cf6b07c1c405
Kolacs <71070272+Kolacs@users.noreply.github.com>
2023-01-23 11:18:17 +0100

    Changed padding in folders table

--- Backend/Services/JwtManager.cs
+++ Backend/Services/JwtManager.cs
@@ -1,5 +1,4 @@
 ﻿using ApiToDatabase.Models.RequestModels;
-using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
