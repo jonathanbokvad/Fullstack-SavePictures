3b7ad98f436eb2c23739954ae22eec20989dcb7f
Jonathan Bokvad Engarås <71070272+jonathanbokvad@users.noreply.github.com>
2023-01-24 19:13:31 +0100

    Delete RunProject.ps1

--- RunProject.ps1
+++
@@ -1,6 +0,0 @@
-winget install Microsoft.DotNet.SDK.6
-cd Client
-npm i live-server
-start powershell {live-server --open=src/index.html --cors}
-cd ..
-start powershell {dotnet run --project backend}
