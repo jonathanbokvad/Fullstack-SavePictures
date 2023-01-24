a0913555359e545529352ed85a385305eb44c967
Kolacs <71070272+Kolacs@users.noreply.github.com>
2023-01-24 17:44:12 +0100

    Added sdk download to script

--- RunProject.ps1
+++ RunProject.ps1
@@ -1,4 +1,6 @@
-start powershell {dotnet run --project backend}
+winget install Microsoft.DotNet.SDK.6
 cd Client
+npm install -g live-server
 start powershell {live-server --open=src/index.html --cors}
 cd ..
+start powershell {dotnet run --project backend}
