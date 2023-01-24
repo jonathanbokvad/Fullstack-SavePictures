06d8b6c44d772f3e9dd1ab7cc35eb66b0f2ff17d
Kolacs <71070272+Kolacs@users.noreply.github.com>
2023-01-24 17:59:08 +0100

    changed script

--- RunProject.ps1
+++ RunProject.ps1
@@ -1,6 +1,6 @@
 winget install Microsoft.DotNet.SDK.6
 cd Client
-npm install -g live-server
+npm i live-server
 start powershell {live-server --open=src/index.html --cors}
 cd ..
 start powershell {dotnet run --project backend}
